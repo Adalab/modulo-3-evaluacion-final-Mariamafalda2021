ced8847efa2ccd08b3e77aec72aea9d1e287aa0e
Mariamafalda2021 <mariarezgarcia@gmail.com>
2022-01-26 19:34:06 +0100

    readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,9 @@ function Header() {
     return (
         <>
             <header>
-                <img src="https://img1.picmix.com/output/stamp/normal/7/0/6/9/629607_e21e1.png" alt="" />
+                <div className="logo">
+                    <img src="https://img1.picmix.com/output/stamp/normal/7/0/6/9/629607_e21e1.png" alt="" />
+                </div>
             </header>
         </>
     )
